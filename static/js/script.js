5992a55b191955c1adabf535c2691dfffb3d65fa
Philip Klostermann <philip.klostermann@gmail.com>
2015-12-19 22:59:14 -0500

    validate URL before submitting

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,50 @@
 $(document).ready(function() {
   $('#scrape-form').submit(submitScrapeForm);
+  $('#yt-url').on('input', validateUrl);
 });
 
 function submitScrapeForm(e) {
-  var ytUrl = $('#yt-url').val();
-  if(!ytUrl) return false;
-  
-  var videoID = extractVideoID(ytUrl);
-  if(!videoID) {
-    alert('Invalid Youtube URL.'); 
-    e.preventDefault();
+  var videoID = validateUrl();
+  if (!videoID) {
+    return;
   }
-  
   $(this).append('<input value="' + videoID + '" name="videoID" id="videoID" class="hidden">');
-  console.log($(this));
+}
+
+function validateUrl() {
+  var url = $('#yt-url').val();
+
+  if (!url) {
+    $('#url-input').removeClass('has-error');
+    $('#url-input').removeClass('has-success');
+    $('#scrape-btn').prop('disabled', true);
+    $('#scrape-btn').removeClass('btn-success');
+    return;
+  }
+
+  var videoID = extractVideoID(url)
+  if (!videoID) {
+    $('#url-input').removeClass('has-success');
+    $('#url-input').addClass('has-error');
+    $('#scrape-btn').prop('disabled', true);
+    $('#scrape-btn').removeClass('btn-success');
+    return;
+  } else {
+    $('#url-input').addClass('has-success');
+    $('#url-input').removeClass('has-error');
+    $('#scrape-btn').prop('disabled', false);
+    $('#scrape-btn').addClass('btn-success');
+    return videoID;
+  }
 }
 
 function extractVideoID(url) {
   if(!url || !url.length || typeof url != 'string')
     return false;
-  
+
   var m = /(?:http[s]?:\/\/)?(?:www\.)?youtube\.\w{2,3}\/watch\?v=([^&]+)\&?/i.exec(url);
   if(!m || m.length < 1)
     return false;
-  
+
   return m[1];
 }
