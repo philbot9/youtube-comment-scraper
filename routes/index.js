e1b13a9c0deb629ffb69e9c8c0ea53101506df41
Philip Klostermann <philip.klostermann@gmail.com>
2019-10-23 11:44:48 -0400

    disable "unavailable" message

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var render = require('../lib/render-template')
 
 module.exports = function (req, res) {
-  // render(res, 200, 'index')
-  render(res, 200, 'unavailable')
+  render(res, 200, 'index')
+  // render(res, 200, 'unavailable')
 }
